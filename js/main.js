ea0fb08c5e770feb5903028349d55e34c276b91a
Vanessa Chimoy <cchimoy@gmail.com>
2023-10-29 13:26:22 -0500

    segunda pre entrega

--- js/main.js
+++ js/main.js
@@ -1,50 +1,75 @@
 let username = prompt("Cual es tu nombre?");
-alert("Bienvenid@ de vuelta a tu lista de juegos " + username);
+alert("Bienvenid@ de vuelta a tu lista de videojuegos, " + username);
 
-let juego1 = "The Legend of Zelda, Tears of the kingdom"
-let juego2 = "Killer Instinct"
-let juego3 = "Super Smash Bros 64"
+const videoGamesList = [];
 
-function mostrarJuegos(){
-    alert(`Tus videojuegos son ${juego1}, ${juego2}, ${juego3}`);
+function mostrarVideojuegos() {
+    if (videoGamesList.length === 0) {
+        alert("Tu lista de videojuegos está vacía.");
+    } else {
+        let message = "Tus videojuegos son:\n";
+        for (let i = 0; i < videoGamesList.length; i++) {
+            message += `${i + 1}. ${videoGamesList[i].nombre} - Precio: ${videoGamesList[i].precio}\n`;
+        }
+        alert(message);
+    }
 }
 
-function agregarNuevoJuego(nuevoJuego) {
-    if (!nuevoJuego){
-        alert("Ingresa el nombre de tu juego")
-    } else{
-        juego3 = nuevoJuego
-        alert(`Has agregado "${nuevoJuego}" a tu lista`)
-    }
+function agregarNuevoVideojuego() {
+    const nombre = prompt("Ingresa el nombre del videojuego:");
+    const precio = parseFloat(prompt("Ingresa el precio del videojuego:"));
     
+    if (!nombre || isNaN(precio)) {
+        alert("Ingresa un nombre y precio válidos.");
+    } else {
+        videoGamesList.push({ nombre, precio });
+        alert(`Has agregado "${nombre}" a tu lista de videojuegos.`);
+    }
+}
+
+function buscarPorRangoDePrecios() {
+    const minPrice = parseFloat(prompt("Ingresa el precio mínimo del rango:"));
+    const maxPrice = parseFloat(prompt("Ingresa el precio máximo del rango:"));
+    
+    if (isNaN(minPrice) || isNaN(maxPrice)) {
+        alert("Ingresa precios válidos.");
+        return;
+    }
+
+    const filteredGames = videoGamesList.filter(game => game.precio >= minPrice && game.precio <= maxPrice);
+    if (filteredGames.length === 0) {
+        alert("No se encontraron juegos en ese rango de precios.");
+    } else {
+        let message = "Juegos en el rango de precios:\n";
+        for (let i = 0; i < filteredGames.length; i++) {
+            message += `${i + 1}. ${filteredGames[i].nombre} - Precio: ${filteredGames[i].precio}\n`;
+        }
+        alert(message);
+    }
 }
 
 function listaDeOpciones() {
     let opcion;
-    do{
-        opcion = parseInt(prompt("Menu:\n" + "1. Mostrar lista de juegos\n" + "2. Agregar nuevo juego\n" + "3. Salir"));
+    do {
+        opcion = parseInt(prompt("Menu:\n" + "1. Mostrar lista de videojuegos\n" + "2. Agregar nuevo videojuego\n" + "3. Buscar por rango de precios\n" + "4. Salir"));
 
-switch (opcion){
-    case 1:
-        mostrarJuegos();
-        break;
-    case 2:
-        let nuevoJuego = prompt("Ingresa l nombre del nuevo juego");
-        agregarNuevoJuego(nuevoJuego);
-        break;
-    case 3:
-        alert("Gracias por revisar tu lista");
-        break
-    default:
-        alert("opcion no valida")
+        switch (opcion) {
+            case 1:
+                mostrarVideojuegos();
+                break;
+            case 2:
+                agregarNuevoVideojuego();
+                break;
+            case 3:
+                buscarPorRangoDePrecios();
+                break;
+            case 4:
+                alert("Gracias por usar la lista de videojuegos.");
+                break;
+            default:
+                alert("Opción no válida.");
+        }
+    } while (opcion !== 4);
 }
 
-
-    } while (opcion !== 3); 
-    
-} 
-
-listaDeOpciones()
-    
-
-
+listaDeOpciones();
