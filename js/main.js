7729510ca75fd56856b3f1949a43333acd10dad7
Vanessa Chimoy <cchimoy@gmail.com>
2023-11-12 10:06:40 -0500

    3ra preentrega

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-// Predefined list of Nintendo 64 games
 const games = [
     { id: 1, title: "Super Mario 64", price: 29.99 },
     { id: 2, title: "The Legend of Zelda: Ocarina of Time", price: 39.99 },
@@ -7,13 +6,10 @@ const games = [
     { id: 5, title: "Banjo-Kazooie", price: 34.99 }
 ];
 
-// Initialize the game list and cart
 function initialize() {
     displayGameList();
-    displayCart();
 }
 
-// Display the list of games
 function displayGameList() {
     const gameListDiv = document.getElementById("gameList");
 
@@ -29,7 +25,6 @@ function displayGameList() {
     });
 }
 
-// Add a game to the cart
 function addToCart(gameId) {
     const selectedGame = games.find(game => game.id === gameId);
 
@@ -41,7 +36,6 @@ function addToCart(gameId) {
     }
 }
 
-// Display the shopping cart
 function displayCart() {
     const cartList = document.getElementById("cartList");
     const totalPriceElement = document.getElementById("totalPrice");
@@ -60,51 +54,39 @@ function displayCart() {
 
     totalPriceElement.textContent = total.toFixed(2);
 
-    // Clear existing buttons
     const existingButtons = document.querySelectorAll("#cart button");
     existingButtons.forEach(button => button.remove());
 
-    // Add "Pagar" button
     const payButton = document.createElement("button");
     payButton.textContent = "Pagar";
     payButton.onclick = () => {
-        // Redirecting to the "thankyou.html" page
-        // window.location.href = "thankyou.html";
-
-        // Displaying a thank you message on the same page
         displayThankYouMessage();
     };
     document.getElementById("cart").appendChild(payButton);
 
-    // Add "Eliminar Juegos" button
     const removeGamesButton = document.createElement("button");
     removeGamesButton.textContent = "Eliminar Juegos";
     removeGamesButton.onclick = removeGamesFromCart;
     document.getElementById("cart").appendChild(removeGamesButton);
 }
 
-// Display a thank you message on the same page
 function displayThankYouMessage() {
     const cartDiv = document.getElementById("cart");
     cartDiv.innerHTML = "<h2>Gracias por su compra</h2>";
 }
 
-// Remove all games from the cart
 function removeGamesFromCart() {
     updateCartInLocalStorage([]);
     displayCart();
 }
 
-// Get the cart from local storage
 function getCartFromLocalStorage() {
     const cartString = localStorage.getItem("shoppingCart");
     return cartString ? JSON.parse(cartString) : [];
 }
 
-// Update the cart in local storage
 function updateCartInLocalStorage(cart) {
     localStorage.setItem("shoppingCart", JSON.stringify(cart));
 }
 
-// Initialize the page
 initialize();
